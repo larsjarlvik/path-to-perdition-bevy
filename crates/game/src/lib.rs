54e9b4565336d89b3c24c37e06fff477dff9bf6c
larsjarlvik <lars.jarlvik@gmail.com>
2023-05-06 00:18:08 +0700

    Fix android build (remove shadows)

--- crates/game/src/lib.rs
+++ crates/game/src/lib.rs
@@ -1,68 +1,37 @@
-use bevy::{math::*, pbr::*, prelude::*};
+use bevy::{math::vec3, pbr::*, prelude::*};
 
 const BACKGROUND_COLOR: Color = Color::rgb(0.0, 0.0, 0.0);
 
 #[bevy_main]
 pub fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins)
-        .insert_resource(Msaa::Sample4)
+    let mut app = App::new();
+    app.add_plugins(DefaultPlugins)
         .insert_resource(ClearColor(BACKGROUND_COLOR))
-        .insert_resource(DirectionalLightShadowMap { size: 4096 })
-        .add_startup_system(setup)
         .add_system(bevy::window::close_on_esc)
-        .run();
+        .add_startup_system(setup_scene);
+
+    app.run();
 }
 
-fn setup(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-    ass: Res<AssetServer>,
-) {
-    let character = ass.load("models/character.glb#Scene0");
-
-    commands.spawn(SceneBundle {
-        scene: character,
-        transform: Transform::from_xyz(0.0, -1.0, 0.0).with_scale(vec3(0.01, 0.01, 0.01)),
-        ..Default::default()
-    });
-
-    commands.spawn((
-        PbrBundle {
-            mesh: meshes.add(Mesh::from(shape::Plane {
-                size: 10.0,
-                subdivisions: 1,
-            })),
-            transform: Transform::from_xyz(0.0, -1.0, 0.0),
-            material: materials.add(StandardMaterial {
-                base_color: Color::WHITE,
-                perceptual_roughness: 0.1,
-                ..default()
-            }),
-            ..default()
-        },
-        NotShadowCaster,
-    ));
+/// set up a simple 3D scene
+fn setup_scene(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn((Camera3dBundle {
+        transform: Transform::from_xyz(5.0, 5.0, 5.0).looking_at(Vec3::new(0.0, 0.0, 0.0), Vec3::Y),
+        ..default()
+    },));
 
     commands.spawn(DirectionalLightBundle {
         directional_light: DirectionalLight {
-            shadows_enabled: true,
             illuminance: 12000.0,
             ..default()
         },
-        cascade_shadow_config: CascadeShadowConfigBuilder {
-            num_cascades: 3,
-            maximum_distance: 50.0,
-            ..default()
-        }
-        .into(),
         transform: Transform::from_rotation(Quat::from_euler(EulerRot::XYZ, -1.0, -0.5, -0.5)),
         ..default()
     });
 
-    commands.spawn(Camera3dBundle {
-        transform: Transform::from_xyz(0.0, 3.0, 6.0).looking_at(Vec3::ZERO, Vec3::Y),
+    commands.spawn(SceneBundle {
+        scene: asset_server.load("models/character.glb#Scene0"),
+        transform: Transform::from_xyz(0.0, -1.0, 0.0).with_scale(vec3(0.01, 0.01, 0.01)),
         ..default()
     });
 }
